d5cb1f6786d8e2ad49c4d89a4d0ba4ab97397cfe
Jon D <120279130+jon-dyer@users.noreply.github.com>
2023-12-31 10:12:55 -0500

    complete

--- dayone/Main.hs
+++ dayone/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import DayOne (sumCalibrations, sumTextyCalis)
-import DayTwo (standardBag, sumPossibleGames)
+import DayTwo (standardBag, sumPossibleGames, sumPowers)
 
 main :: IO ()
 main = do
@@ -10,3 +10,4 @@ main = do
   print (show (sumCalibrations dayOneContent))
   print (show (sumTextyCalis dayOneContent))
   print (show (sumPossibleGames standardBag dayTwoContent))
+  print (show (sumPowers dayTwoContent))
